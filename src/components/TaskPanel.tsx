2825b758965d0d37d4ac018e5d027813864b8c3d
Johannes Millan <johannes.millan@gmail.com>
2026-02-17 12:12:55 +0100

    style: replace colored branch/folder text with SVG icons
    Use git-branch and folder SVG icons instead of accent-colored
    text in the task info bar.

--- src/components/TaskPanel.tsx
+++ src/components/TaskPanel.tsx
@@ -99,10 +99,18 @@ export function TaskPanel(props: TaskPanelProps) {
           title={props.task.worktreePath}
           onClick={() => revealItemInDir(props.task.worktreePath).catch(() => {})}
         >
-          <span style={{ "margin-right": "8px", color: theme.accent }}>
+          <span style={{ display: "inline-flex", "align-items": "center", gap: "4px", "margin-right": "12px" }}>
+            <svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor" style={{ "flex-shrink": "0" }}>
+              <path d="M5 3.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm6.25 7.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM5 7.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm0 0h5.5a2.5 2.5 0 0 0 2.5-2.5v-.5a.75.75 0 0 0-1.5 0v.5a1 1 0 0 1-1 1H5a3.25 3.25 0 1 0 0 6.5h6.25a.75.75 0 0 0 0-1.5H5a1.75 1.75 0 1 1 0-3.5Z" />
+            </svg>
             {props.task.branchName}
           </span>
-          <span style={{ opacity: 0.6 }}>{props.task.worktreePath}</span>
+          <span style={{ display: "inline-flex", "align-items": "center", gap: "4px", opacity: 0.6 }}>
+            <svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor" style={{ "flex-shrink": "0" }}>
+              <path d="M1.75 1A1.75 1.75 0 0 0 0 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0 0 16 13.25v-8.5A1.75 1.75 0 0 0 14.25 3H7.5a.25.25 0 0 1-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75Z" />
+            </svg>
+            {props.task.worktreePath}
+          </span>
         </InfoBar>
       ),
     };
